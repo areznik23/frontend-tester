83c02b507ba0caddf4d545194f1266dac67d41fa
Alex Reznik <alexreznik23@gmail.com>
2020-12-28 17:38:25 -0600

    consuming the API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,11 @@
+import { useEffect } from 'react'
 const App = () => {
+
+	useEffect(() => {
+		fetch("https://testing-lynx-api.herokuapp.com/hello")
+			.then(res => console.log(res)
+	}, [])
+	
 	return (
 		<p>This is a react application</p>
 	)
